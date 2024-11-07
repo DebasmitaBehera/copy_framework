8f31f1ea2bbd2348a8fc3719876bcd932d687fe9
admin <admin@DESKTOP-E2JOFU2>
2024-11-07 10:07:30 +0530

    added

---
+++ git_framework/src/test/java/git_framework/Lead_Page.java
@@ -0,0 +1,10 @@
+package git_framework;
+
+public class Lead_Page {
+
+	public static void main(String[] args) {
+		System.out.println("Lead page created");
+
+	}
+
+}
---
+++ git_framework/src/test/java/git_framework/TrialClass.java
@@ -0,0 +1,10 @@
+package git_framework;
+
+public class TrialClass {
+
+	public static void main(String[] args) {
+	System.out.println("hello GITHUB by smita");
+
+	}
+
+}
